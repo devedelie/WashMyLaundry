ff643074b7b80647d63653e892ac37a6a0d4d446
e210564e <devedelie@gmail.com>
2020-02-05 22:28:40 +0100

    - Added Provider.class and ProviderHelper.class to separate USERS from PROVIDERS in database

---
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/api/ProviderHelper.java
@@ -0,0 +1,37 @@
+package com.elbaz.eliran.washmylaundry.api;
+
+import com.elbaz.eliran.washmylaundry.models.Provider;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/**
+ * Created by Eliran Elbaz on 04-Feb-20.
+ */
+public class ProviderHelper {
+    private static final String COLLECTION_NAME = "providers";
+
+    // --- COLLECTION REFERENCE ---
+
+    public static CollectionReference getProvidersCollection(){
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+    }
+
+    // --- CREATE ---
+
+    public static Task<Void> createProvider (String pid, String providerName, String urlPicture, boolean isProvider) {
+        // Create Provider object
+        Provider providerToCreate = new Provider(pid, providerName, urlPicture, isProvider);
+        // Add a new Provider Document to Firestore
+        return ProviderHelper.getProvidersCollection()
+                .document(pid) // Setting uID for Document
+                .set(providerToCreate); // Setting object for Document
+    }
+
+    // --- GET (Read) ---
+
+    public static Task<DocumentSnapshot> getProvider(String uid){
+        return ProviderHelper.getProvidersCollection().document(uid).get();
+    }
+}
---
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/models/Provider.java
@@ -0,0 +1,67 @@
+package com.elbaz.eliran.washmylaundry.models;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by Eliran Elbaz on 04-Feb-20.
+ */
+public class Provider {
+    private String pid;
+    private String providerName;
+    @Nullable
+    private String urlPicture, providerAddress;
+    private boolean isProvider, isProviderAvailable;
+    private int providerZipCode;
+    private LatLng providerCoordinates;
+
+    public Provider(){ }
+
+    public Provider(String pid, String providerName, @Nullable String urlPicture, boolean isProvider) {
+        this.pid = pid;
+        this.providerName = providerName;
+        this.urlPicture = urlPicture;
+        this.providerAddress = "";
+        this.isProvider = isProvider;
+        this.isProviderAvailable = false;
+        this.providerZipCode = 0;
+        this.providerCoordinates = null;
+    }
+
+    public String getPid() { return pid; }
+
+    public void setPid(String pid) { this.pid = pid; }
+
+    public String getProviderName() { return providerName; }
+
+    public void setProviderName(String providerName) { this.providerName = providerName; }
+
+    @Nullable
+    public String getUrlPicture() { return urlPicture; }
+
+    public void setUrlPicture(@Nullable String urlPicture) { this.urlPicture = urlPicture; }
+
+    @Nullable
+    public String getProviderAddress() { return providerAddress; }
+
+    public void setProviderAddress(@Nullable String providerAddress) { this.providerAddress = providerAddress; }
+
+    public boolean getIsProvider() { return isProvider; }
+
+    public void setIsProvider(boolean provider) { isProvider = provider; }
+
+    public boolean getIsProviderAvailable() { return isProviderAvailable; }
+
+    public void setIsProviderAvailable(boolean providerAvailable) { isProviderAvailable = providerAvailable; }
+
+    public int getProviderZipCode() { return providerZipCode; }
+
+    public void setProviderZipCode(int providerZipCode) { this.providerZipCode = providerZipCode; }
+
+    public LatLng getProviderCoordinates() { return providerCoordinates; }
+
+    public void setProviderCoordinates(LatLng providerCoordinates) { this.providerCoordinates = providerCoordinates; }
+}
+
+
