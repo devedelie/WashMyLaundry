b026ea291f6a5195029f1ddfeb46fa2087654d87
e210564e <devedelie@gmail.com>
2020-02-05 22:27:23 +0100

    - Fixed a logcat common error with CustomeGlideModule - Added google.gms plugin support

---
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/api/CustomeGlideModule.java
@@ -0,0 +1,14 @@
+package com.elbaz.eliran.washmylaundry.api;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Eliran Elbaz on 04-Feb-20.
+ */
+// Solves Error from Logcat
+@GlideModule
+public class CustomeGlideModule extends AppGlideModule {
+}
+
+
