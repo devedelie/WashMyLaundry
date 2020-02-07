74e8636325b1d25a1996e1556d0f99b37dec3294
e210564e <devedelie@gmail.com>
2020-02-07 12:13:44 +0100

    - Modified edit_provider design - Added Address AutoComplete functionality to fetch address and LatLng object from google

--- app/src/main/java/com/elbaz/eliran/washmylaundry/base/BaseBottomSheet.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/base/BaseBottomSheet.java
@@ -2,22 +2,31 @@ package com.elbaz.eliran.washmylaundry.base;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import com.elbaz.eliran.washmylaundry.R;
+import com.elbaz.eliran.washmylaundry.controllers.activities.MainActivity;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import butterknife.ButterKnife;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 06-Feb-20.
@@ -96,6 +105,37 @@ public abstract class BaseBottomSheet extends BottomSheetDialogFragment {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         return displayMetrics.heightPixels;
+    }
+
+    // --------------------
+    // NETWORK CONNECTIVITY
+    // --------------------
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager)  getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        Log.d(TAG, "isNetworkAvailable: " + activeNetworkInfo);
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public AlertDialog displayMobileDataSettingsDialog(final Activity activity, final Context context){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("No Internet");
+        builder.setMessage("Please enable network access");
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+        return builder.create();
     }
 
 }
--- app/src/main/java/com/elbaz/eliran/washmylaundry/controllers/fragments/bottomSheets/EditProviderBottomSheet.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/controllers/fragments/bottomSheets/EditProviderBottomSheet.java
@@ -1,16 +1,52 @@
 package com.elbaz.eliran.washmylaundry.controllers.fragments.bottomSheets;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import androidx.annotation.Nullable;
+
+import com.elbaz.eliran.washmylaundry.BuildConfig;
 import com.elbaz.eliran.washmylaundry.R;
 import com.elbaz.eliran.washmylaundry.base.BaseBottomSheet;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
 import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 06-Feb-20.
  */
 public class EditProviderBottomSheet extends BaseBottomSheet {
+    @BindView(R.id.edit_provider_address_text) EditText addressEditText;
+    @BindView(R.id.edit_provider_zip_text) EditText zipEditText;
+    @BindView(R.id.edit_provider_phone_text) EditText phoneEditText;
+    @BindView(R.id.edit_provider_machine_type_text) EditText machineEditText;
+
+    // For DATA
+    private int AUTOCOMPLETE_REQUEST_CODE = 1;
+    private List<Place.Field> mFields = Arrays.asList(Place.Field.ID, Place.Field.ADDRESS,Place.Field.LAT_LNG); // Set the fields to specify which types of place data to return after the user has made a selection.
+    private LatLng addressLatLng;
 
     public static EditProviderBottomSheet newInstance() {
         EditProviderBottomSheet editProviderBottomSheet;
@@ -22,6 +58,12 @@ public class EditProviderBottomSheet extends BaseBottomSheet {
         return editProviderBottomSheet ;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.configureAddressViewType();
+    }
+
     // --------------------
     // CONFIGURATIONS
     // --------------------
@@ -31,6 +73,23 @@ public class EditProviderBottomSheet extends BaseBottomSheet {
     @Override
     protected int setTitle() { return R.string.edit_bs_title; }
 
+    private void configureAddressViewType() {
+        addressEditText.setClickable(true);
+        addressEditText.setFocusable(false);
+        addressEditText.setInputType(InputType.TYPE_NULL);
+        addressEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isNetworkAvailable()){
+                    launchAutocompleteSearchBar(); // If Internet is active - then invoke Google Address-Autocomplete
+                }else {
+//                    alertDialogAddress(); // If Internet is inactive - invoke warning dialog box
+                }
+            }
+        });
+    }
+
+
     // --------------------
     // ACTIONS
     // --------------------
@@ -38,6 +97,47 @@ public class EditProviderBottomSheet extends BaseBottomSheet {
     @OnClick(R.id.bottom_sheet_back_button)
     public void onBackButtonClick(){
         dismiss();
+    }
+
+    private void launchAutocompleteSearchBar(){
+        if (!Places.isInitialized()) {
+            Places.initialize(getActivity().getApplicationContext(), BuildConfig.GOOGLE_API_KEY, Locale.US);
+        }
+        // Bias results to Paris region (use 'bounds' variable in below filter)
+        RectangularBounds bounds = RectangularBounds.newInstance(
+                new LatLng(48.832304, 2.239726),
+                new LatLng(48.900962, 2.42124));
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.OVERLAY, mFields)
+                .setTypeFilter(TypeFilter.ADDRESS)
+                .setCountry("Fr")
+                .setLocationBias(bounds)
+                .build(getActivity().getApplicationContext());
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    // onActivityResult for Search Auto-Complete
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                //Manage the action to be taken
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                addressEditText.setText(place.getAddress());
+                addressLatLng = place.getLatLng();
+                Log.d(TAG, "onActivityResult: " + addressLatLng);
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i(ContentValues.TAG, "onActivityResult Error: " + status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 
 }
