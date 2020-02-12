5b93674aee76cc270492876fdf3bc7713dc95025
e210564e <devedelie@gmail.com>
2020-02-12 10:45:14 +0100

    - Made the viewModel call the Firestore-Listener API through a Repository

--- app/src/main/java/com/elbaz/eliran/washmylaundry/repositories/ProviderDataRepository.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/repositories/ProviderDataRepository.java
@@ -1,8 +1,18 @@
 package com.elbaz.eliran.washmylaundry.repositories;
 
+import android.util.Log;
+
+import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
-import com.elbaz.eliran.washmylaundry.models.User;
+import com.elbaz.eliran.washmylaundry.api.ProviderHelper;
+import com.elbaz.eliran.washmylaundry.models.Provider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 07-Feb-20.
@@ -11,7 +21,8 @@ public class ProviderDataRepository {
 
     private static ProviderDataRepository sInstance;
 
-    private MutableLiveData<User> mProviderData = new MutableLiveData<>();
+    private MutableLiveData<Provider> mProviderData = new MutableLiveData<>();
+    private Provider mProvider;
 
     // Singleton
     public static ProviderDataRepository getInstance(){
@@ -28,7 +39,8 @@ public class ProviderDataRepository {
 
     // --- GET ---
 
-    public MutableLiveData<User> getCurrentProviderData(){
+    public MutableLiveData<Provider> getCurrentProviderData(){
+        Log.d(TAG, "setCurrentProviderData: 1");
         return mProviderData;
     }
 
@@ -36,9 +48,39 @@ public class ProviderDataRepository {
 
     // --- SET ---
 
-    public void setCurrentProviderData(User user) {
-        mProviderData.setValue(user);
+    public void setCurrentProviderData() {  // 'Set' behaves as a trigger for the listener
+        Log.d(TAG, "setCurrentProviderData: 1");
+        userDataFirestoreListener();
     }
 
 
+
+    //--------------------------
+    // Cloud Firestore Listeners
+    //--------------------------
+    private void userDataFirestoreListener(){
+        final DocumentReference docRef = ProviderHelper.getProviderDocument(CurrentUserDataRepository.currentUserID);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+                // -- Data received
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d(TAG, "Current data: " + snapshot.getData());
+                    mProvider = new Provider();
+                    mProvider = snapshot.toObject(Provider.class);
+                    if(mProvider !=null){
+                        mProviderData.setValue(mProvider); // If a change was detected, set Object in ViewModel
+                    }
+
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/com/elbaz/eliran/washmylaundry/viewmodel/ProviderViewModel.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/viewmodel/ProviderViewModel.java
@@ -4,7 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.elbaz.eliran.washmylaundry.models.User;
+import com.elbaz.eliran.washmylaundry.models.Provider;
+import com.elbaz.eliran.washmylaundry.repositories.ProviderDataRepository;
 
 /**
  * Created by Eliran Elbaz on 07-Feb-20.
@@ -12,28 +13,28 @@ import com.elbaz.eliran.washmylaundry.models.User;
 public class ProviderViewModel extends ViewModel {
 
     // REPOSITORIES
-//    private ProviderDataRepository mProviderDataRepository;
+    private ProviderDataRepository mProviderDataRepository = ProviderDataRepository.getInstance();
 
     // Data
-    private MutableLiveData<User> mUserMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<Provider> mUserMutableLiveData = new MutableLiveData<>();
 
-//    public void init(){
-//        if(mUserMutableLiveData != null){
-//            return;
-//        }
-//        mProviderDataRepository = ProviderDataRepository.getInstance();
-//        mUserMutableLiveData = mProviderDataRepository.getCurrentProviderData();
-//    }
-
-
-    public LiveData<User> getCurrentProviderData(){
-        return mUserMutableLiveData;
-//        return mProviderDataRepository.getCurrentProviderData();
+    public void init(){
+        if(mUserMutableLiveData != null){
+            return;
+        }
+        mProviderDataRepository = ProviderDataRepository.getInstance();
+        mUserMutableLiveData = mProviderDataRepository.getCurrentProviderData();
     }
 
-    public void setCurrentProviderData(User user){
-        this.mUserMutableLiveData.setValue(user);
-//        mProviderDataRepository.setCurrentProviderData(user);
+
+    public LiveData<Provider> getCurrentProviderData(){
+//        return mUserMutableLiveData;
+        return mProviderDataRepository.getCurrentProviderData();
+    }
+
+    public void setCurrentProviderData(){
+//        this.mUserMutableLiveData.setValue(provider);
+        mProviderDataRepository.setCurrentProviderData();
     }
 
 }
