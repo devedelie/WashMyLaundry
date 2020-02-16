0e18b802725f70896591996027200e35c7f4232c
e210564e <devedelie@gmail.com>
2020-02-16 18:44:35 +0100

    - Orders model modification

--- app/src/main/java/com/elbaz/eliran/washmylaundry/models/Orders.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/models/Orders.java
@@ -4,12 +4,11 @@ package com.elbaz.eliran.washmylaundry.models;
  * Created by Eliran Elbaz on 08-Feb-20.
  */
 public class Orders {
-    private String uid;
+    private String uid, pid, uniqueOrderId;
 
-    public Orders() {
-    }
+    public Orders() {}
 
-    public Orders(String uid) { // For creating an empty collection for provider's personal-orders
+    public Orders(String uid) {
         this.uid = uid;
     }
 
