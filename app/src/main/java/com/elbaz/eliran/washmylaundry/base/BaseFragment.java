628e94b6968e2de37a824f1c8f067c084b918b02
e210564e <devedelie@gmail.com>
2020-02-16 15:04:09 +0100

    - Added alertDialog for BaseFragment

--- app/src/main/java/com/elbaz/eliran/washmylaundry/base/BaseFragment.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/base/BaseFragment.java
@@ -1,5 +1,6 @@
 package com.elbaz.eliran.washmylaundry.base;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +9,9 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.elbaz.eliran.washmylaundry.R;
 import com.elbaz.eliran.washmylaundry.models.User;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -67,4 +70,24 @@ public abstract class BaseFragment extends Fragment {
 
     @Nullable
     public FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    // -------------
+    // AlertDialogs
+    // -------------
+
+    public void alertDialogAction(String title, String message){
+        new MaterialAlertDialogBuilder(getContext().getApplicationContext(), R.style.ThemeOverlay_MaterialComponents_MaterialAlertDialog)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Ok and close the alert
+
+                    }
+                })
+                // A null listener allows the button to dismiss the dialog and take no further action.
+//                .setNegativeButton(android.R.string.no, null)
+                .setIcon(R.drawable.ic_help_black_24dp)
+                .show();
+    }
 }
