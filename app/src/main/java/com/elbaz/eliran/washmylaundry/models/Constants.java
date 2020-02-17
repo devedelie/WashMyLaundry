76f7db155bbf6a9f0d2561050e3494c3ee3e2e8c
e210564e <devedelie@gmail.com>
2020-02-17 18:40:14 +0100

    - Added Google Static Map API url into constants

--- app/src/main/java/com/elbaz/eliran/washmylaundry/models/Constants.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/models/Constants.java
@@ -6,4 +6,8 @@ package com.elbaz.eliran.washmylaundry.models;
 public class Constants {
     public static final float MINIMUM_ZOOM_PREFERENCE = 14.5f;
     public static final float MAXIMUM_ZOOM_PREFERENCE = 18.0f;
+
+    public static final String IMAGE_URL_PART1 = "https://maps.googleapis.com/maps/api/staticmap?center=";
+    public static final String IMAGE_URL_PART2 = "&zoom=15&size=120x120&maptype=roadmap&markers=color:blue%7Clabel:S%7C";
+    public static final String IMAGE_URL_PART3 = "&key=";
 }
