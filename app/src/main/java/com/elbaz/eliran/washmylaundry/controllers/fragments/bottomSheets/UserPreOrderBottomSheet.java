cabbe9dedd637aaa3ecee28f851eb3095ffe7090
e210564e <devedelie@gmail.com>
2020-02-17 18:35:15 +0100

    - Added service rate TextView - Switches Switches with CheckBoxes - Added condition to allow order only when user updated his Address on profile

--- app/src/main/java/com/elbaz/eliran/washmylaundry/controllers/fragments/bottomSheets/UserPreOrderBottomSheet.java
+++ app/src/main/java/com/elbaz/eliran/washmylaundry/controllers/fragments/bottomSheets/UserPreOrderBottomSheet.java
@@ -6,16 +6,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
+
+import androidx.lifecycle.Observer;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.elbaz.eliran.washmylaundry.R;
 import com.elbaz.eliran.washmylaundry.controllers.activities.ReservationActivity;
 import com.elbaz.eliran.washmylaundry.models.Provider;
+import com.elbaz.eliran.washmylaundry.models.User;
+import com.elbaz.eliran.washmylaundry.repositories.UserDataRepository;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.gson.Gson;
 
@@ -32,11 +36,12 @@ public class UserPreOrderBottomSheet extends BottomSheetDialogFragment {
     @BindView(R.id.id_details_profile_image) ImageView providerImage;
     @BindView(R.id.id_details_username_text) TextView providerName;
     @BindView(R.id.id_details_services_counter_text) TextView providerServiceCounter;
+    @BindView(R.id.id_details_services_rate_text) TextView providerServiceRate;
     @BindView(R.id.pre_order_about_me_text) TextView providerDescription;
     @BindView(R.id.pre_order_plus_minus_number) TextView bagsNumber;
     @BindView(R.id.pre_order_max_bags_text) TextView bagsNumberText;
-    @BindView(R.id.pre_order_ironing_switch) Switch ironingSwitch;
-    @BindView(R.id.pre_order_delivery_switch) Switch deliverySwitch;
+    @BindView(R.id.pre_order_ironing_switch) CheckBox ironingCheckBox;
+    @BindView(R.id.pre_order_delivery_switch) CheckBox deliveryCheckBox;
 
     // For Data
     private Provider mProvider = new Provider();
@@ -44,6 +49,7 @@ public class UserPreOrderBottomSheet extends BottomSheetDialogFragment {
     // Data for Order
     private int bagsNumberInt =1; // Default bags number for order
     private boolean isIroningChecked, isDeliveryChecked;
+    private User mUser = new User();
 
     public static UserPreOrderBottomSheet newInstance(String key, String providerJson) {
         Log.d(TAG, "newInstance BottomSheetEditProvider: " + key + " " +providerJson);
@@ -65,8 +71,14 @@ public class UserPreOrderBottomSheet extends BottomSheetDialogFragment {
         mProvider = new Gson().fromJson(jsonObject, Provider.class);
         Log.d(TAG, "onCreateView: BTMS " + mProvider.getProviderName());
 //
-        this.configureSwitches();
+        this.configureCheckBoxes();
         this.setUiElements();
+        UserDataRepository.getInstance().getCurrentUserData().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                mUser = user;
+            }
+        });
 
         return view;
     }
@@ -74,18 +86,18 @@ public class UserPreOrderBottomSheet extends BottomSheetDialogFragment {
     // --------------------
     // Configurations
     // --------------------
-    private void configureSwitches() {
+    private void configureCheckBoxes() {
         // ----- Ironing Switch -------//
-        ironingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        ironingCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(ironingSwitch.isChecked()){ isIroningChecked = true; }
+                if(ironingCheckBox.isChecked()){ isIroningChecked = true; }
                 else{ isIroningChecked = false; }
             }
         });
         // ----- Delivery Switch -------//
-        deliverySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        deliveryCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(deliverySwitch.isChecked()){ isDeliveryChecked = true; }
+                if(deliveryCheckBox.isChecked()){ isDeliveryChecked = true; }
                 else{ isDeliveryChecked = false; }
             }
         });
@@ -111,12 +123,16 @@ public class UserPreOrderBottomSheet extends BottomSheetDialogFragment {
 
     @OnClick(R.id.edit_provider_continue_btn)
     public void onContinueBtnClick (){
-        Intent intent = new Intent(getActivity().getApplicationContext(), ReservationActivity.class);
-        intent.putExtra("isDeliveryChecked", isDeliveryChecked);
-        intent.putExtra("isIroningChecked", isIroningChecked);
-        intent.putExtra("bagsNumber", bagsNumberInt);
-        intent.putExtra("providerObject", new Gson().toJson(mProvider));
-        startActivity(intent);
+        if(mUser.getUserAddress() != null && !mUser.getUserAddress().isEmpty()){
+            Intent intent = new Intent(getActivity().getApplicationContext(), ReservationActivity.class);
+            intent.putExtra("isDeliveryChecked", isDeliveryChecked);
+            intent.putExtra("isIroningChecked", isIroningChecked);
+            intent.putExtra("bagsNumber", bagsNumberInt);
+            intent.putExtra("providerObject", new Gson().toJson(mProvider));
+            startActivity(intent);
+        }else{
+            Log.d(TAG, "onContinueBtnClick: "); // invoke error dialog to set address before reservation
+        }
     }
     // --------------------
     // UI
@@ -132,6 +148,7 @@ public class UserPreOrderBottomSheet extends BottomSheetDialogFragment {
 
         providerName.setText(mProvider.getProviderName());
         providerServiceCounter.setText(getString(R.string.pre_order_provider_service_count,String.valueOf(mProvider.getServicesCount())));
+        providerServiceRate.setText(getString(R.string.id_details_euro_kg, String.valueOf(mProvider.getPricePerKg())));
         providerDescription.setText(mProvider.getProviderDescription());
         bagsNumberText.setText(getString(R.string.max_bags_15kg, String.valueOf(mProvider.getMaxBags())));
         bagsNumber.setText(String.valueOf(bagsNumberInt));
